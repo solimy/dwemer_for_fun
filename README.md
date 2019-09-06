d1b16dc8ce853e2125d7697b4def7e59405538aa
Hadrien DAURES <hadrien.daures@gmail.com>
2019-09-06 15:13:42 +0200

    feat: tesseract ocr integration to read dwemer from image

---
+++ README.md
@@ -0,0 +1,4 @@
+# Requirements:
+
+- [apt-get | apk | brew | ...] install tesseract
+- pip install -r requirements.txt
--- dwemer.py
+++ dwemer.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 import click
+import pytesseract
 
 __fonts = {
     'normal': 'Dwemer',
@@ -13,14 +14,17 @@ __fonts = {
 @click.option('--write', 'text', default=None)
 @click.option('--font-type', 'font_type', type=click.Choice(__fonts.keys()), default='normal')
 @click.option('--font-size', 'font_size', type=int, default=15)
-def main(text, font_type, font_size):
-    if not text:
-        return
-    img = Image.new('RGB', (((font_size * len(text)) // 2) + font_size, font_size), color = (255, 255, 255))
-    fnt = ImageFont.truetype(f'font/{__fonts[font_type]}.otf', font_size)
-    d = ImageDraw.Draw(img)
-    d.text((font_size // 2,0), text, font=fnt, fill=(0, 0, 0))
-    img.save('dwemer.png')
+@click.option('--read', 'image_file', default=None)
+def main(text, font_type, font_size, image_file):
+    if text:
+        img = Image.new('RGB', (((font_size * len(text)) // 2) + font_size, font_size), color = (255, 255, 255))
+        fnt = ImageFont.truetype(f'font/{__fonts[font_type]}.otf', font_size)
+        d = ImageDraw.Draw(img)
+        d.text((font_size // 2,0), text, font=fnt, fill=(0, 0, 0))
+        img.save('dwemer.png')
+    if image_file:
+        text = pytesseract.image_to_string(Image.open('dwemer.png'), lang='Dwemer', config=r'--tessdata-dir tesseract_data')
+        print(text)
 
 if __name__ == "__main__":
     main()
